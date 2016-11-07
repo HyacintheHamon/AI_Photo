165c7ecd253e2872fcafdb78396a727e67a6b549
Jacob Siegel <jacobsiegel35@gmail.com>
2016-11-06 22:35:19 -0500

    init

---
+++ index.ios.js
@@ -0,0 +1,35 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+import styles from './src/styles.js';
+
+export default class AI_Photo extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit index.ios.js
+        </Text>
+        <Text style={styles.instructions}>
+          Press Cmd+R to reload,{'\n'}
+          Cmd+D or shake for dev menu
+        </Text>
+      </View>
+    );
+  }
+}
+
+AppRegistry.registerComponent('AI_Photo', function() { return AI_Photo });
