00d9df8c5c27feb7a99d3c3df8ae75478fc12296
Jacob Siegel <jacobsiegel35@gmail.com>
2016-11-10 19:26:14 -0500

    Can't seem to figure out how to send the image to microsoft. What the fuckgit add -Agit add -Agit add -A

--- index.ios.js
+++ index.ios.js
@@ -44,42 +44,59 @@ export default class AI_Photo extends Component {
     };
   }
 
-  getNameOfPicture(uri) {
+  convertToAppropriateJson(obj) {
+    const json = JSON.stringify(obj);
+    return json;
+  }
+
+  getNameOfPicture(uri: string) {
     let headers = new Headers();
     headers.append('Content-Type', 'application/json');
     headers.append('Ocp-Apim-Subscription-Key', 'c3fd9e451da54cb7a1327ea21c1c182e');
     const init = {
       method: 'POST',
-      body: '{"url":'+uri+'}',
+      body: this.convertToAppropriateJson( {url:uri} ),
       headers: headers
     };
+
+    console.log(init.body);
     const myRequest = new Request('https://api.projectoxford.ai/vision/v1.0/analyze?', init);
 
     // i think uri is causing error
     fetch(myRequest)
       .then((response) => {
-        console.log(response.json());
-        return response.json();
+        if (!response.ok) {
+          throw Error('AYYYoooo')
+        } else {
+          return response.json();
+        }
       })
-      .then(json => json.categories[0].name)
+      .then((json) => {
+        return json.categories[0].name;
+      })
       .then((name) => {
         this.setState({text:name});
         return name;
       })
       .then((name) => {
-        speech.speak({
-          text: 'Your photo is of a ' + name,
-          voice: 'en-US'
-        });
+        // speech.speak({
+        //   text: 'Your photo is of a ' + name,
+        //   voice: 'en-US'
+        // });
       })
       .catch((error) => { console.error(error) });
   }
 
   launchImagePicker() {
-    ImagePicker.showImagePicker(null, (response) => {
-      const uri = response.uri;
-      this.setState({pic: {uri:uri}});
-      this.getNameOfPicture(uri);
+    const options = {
+      quality: 0.5
+    };
+    ImagePicker.showImagePicker(options, (response) => {
+      let res = response;
+      res.width = 100;
+      res.height = 100;
+      console.log(response);
+      this.setState({pic: {uri:res.origURL}});
     });
   }
 
@@ -95,6 +112,7 @@ export default class AI_Photo extends Component {
             <Image style={{width: 193, height: 110}} source={this.state.pic} />
           </TouchableHighlight>
           <Text>{this.state.text}</Text>
+          <Button onPress={()=>this.getNameOfPicture(this.state.pic.uri)}>Send data</Button>
         </Content>
       </Container>
     );
