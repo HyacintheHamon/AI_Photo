771acea6d1e79c0afaf669d141420aeeab2c369a
Jacob Siegel <jacobsiegel35@gmail.com>
2016-11-16 12:28:31 -0500

    Continuing development

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 /**
  * Sample React Native App
  * https://github.com/facebook/react-native
- *
+ * @flow
  */
  'use strict';
 
@@ -52,13 +52,13 @@ export default class AI_Photo extends Component {
 
   talkToHuman(text) {
     speech.speak({
-      text: 'Your photo is of a ' + text,
+      text: 'Your photo is of ' + text,
       voice: 'en-US'
     });
   }
 
   microsoftApiFetch(rawImageBinary) {
-    const microsoftUrl = 'https://api.projectoxford.ai/vision/v1.0/analyze?';
+    const microsoftUrl = 'https://api.projectoxford.ai/vision/v1.0/analyze?visualFeatures=Categories,Tags,Description,Faces,ImageType,Color,Adult&details=Celebrities&language=en';
     const init = {
       'Accept': 'application/json',
       'Content-Type': 'application/octet-stream',
@@ -66,11 +66,14 @@ export default class AI_Photo extends Component {
     };
 
     RNFetchBlob.fetch('POST', microsoftUrl, init, rawImageBinary)
-      .then(response => response.json())
-      .then(json => json.categories[0].name)
+      .then((response) => {
+        return response.json();
+      })
+      .then((json) => {
+        return json.description.captions[0].text;
+      })
       .then((name) => {
         this.setState({text:name});
-        console.log(name);
         return name;
       })
       .then((text) => {
@@ -139,3 +142,65 @@ const buttonStyle = StyleSheet.create({
     color: '#FFF',
   },
 });
+
+/*
+{
+  categories: [{
+    name: 'outdoor_water',
+    score: 0.828125
+  }],
+  adult:
+   { isAdultContent: false,
+     isRacyContent: false,
+     adultScore: 0.007351919077336788,
+     racyScore: 0.012373056262731552 },
+  tags:
+   [ { name: 'outdoor', confidence: 0.9959841966629028 },
+     { name: 'mountain', confidence: 0.9949145317077637 },
+     { name: 'nature', confidence: 0.9926238059997559 },
+     { name: 'sky', confidence: 0.9898942112922668 },
+     { name: 'grass', confidence: 0.9620691537857056 },
+     { name: 'hill', confidence: 0.7776529788970947 },
+     { name: 'waterfall', confidence: 0.7644999623298645 },
+     { name: 'hillside', confidence: 0.6875730156898499 },
+     { name: 'canyon', confidence: 0.6577474474906921 },
+     { name: 'dirt', confidence: 0.2712985873222351 },
+     { name: 'lush', confidence: 0.19674451649188995 } ],
+  description:
+   { tags:
+      [ 'outdoor',
+        'mountain',
+        'nature',
+        'grass',
+        'hill',
+        'waterfall',
+        'hillside',
+        'side',
+        'water',
+        'rocky',
+        'narrow',
+        'river',
+        'train',
+        'path',
+        'trail',
+        'going',
+        'green',
+        'dirt',
+        'country',
+        'standing',
+        'sheep',
+        'man',
+        'grassy',
+        'large',
+        'traveling',
+        'forest',
+        'track',
+        'field',
+        'grazing',
+        'herd',
+        'riding',
+        'slope' ],
+     captions:
+      [ { text: 'a train on a rocky hill',
+          confidence: 0.4519705464105171 } ] },
+*/
